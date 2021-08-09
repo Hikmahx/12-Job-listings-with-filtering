08c96d95f9d6719ec5e0f2215e11e75d31d411a4
Hikmahx <hikmayousuph@gmail.com>
2021-08-08 21:41:25 -0800

    new method filterSelection added to app.js

--- app.js
+++ app.js
@@ -7,13 +7,5 @@ const ui = new UI;
 dataJson.getData()
 .then(data=>{
     ui.showList(data);
-    
-  let buttons = document.querySelectorAll('.filter button');
-  buttons.forEach(button => {
-    button.addEventListener('click', ()=>{
-      console.log(button)
-      // console.log(data)
-      
-    })
-  });
+    ui.filterSelection();
 })
